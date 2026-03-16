4018704301499fe64780e1ffb5ba0bc709707f97
James Acero <jacero43464@liceo.edu.ph>
2026-03-17 01:37:27 +0800

    Update post.controller.ts

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,7 +1,6 @@
 import type { Context } from "hono";
 
 export const getPosts = async (c: Context) => {
-    // ITO AY EMERGENCY DUMMY DATA PARA SA DEMO
     return c.json([
         { id: 1, title: "Proyektong Backend", content: "Gumagana ang API route!" },
         { id: 2, title: "Test Success", content: "JSON response is working." }
