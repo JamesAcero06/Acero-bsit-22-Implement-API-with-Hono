cf7f41f0dc135d9c5ab03b5d8491242378f83949
James Acero <jacero43464@liceo.edu.ph>
2026-03-17 01:37:04 +0800

    Update post.routes.ts

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,9 +1,8 @@
 import { Hono } from "hono";
-import { getPosts } from "../controllers/post.controller.js"; // Siguraduhing tama ang path
+import { getPosts } from "../controllers/post.controller.js"; 
 
 const postRoutes = new Hono();
 
-// Ito ang nagkakabit ng path sa logic ng controller
 postRoutes.get("/", getPosts); 
 
 export default postRoutes;
