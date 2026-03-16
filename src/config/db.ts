59d90bb72f9f597d02f33e8b66f842094cec48e4
James Acero <jacero43464@liceo.edu.ph>
2026-03-17 01:37:49 +0800

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,8 @@
 const dbConfig = {
     host: 'localhost',
     port: 3306,
-    user: 'root',      // Palitan ang 'JamesAcero' ng 'root'
-    password: '',      // Gawing empty string kung walang password
+    user: 'root',      
+    password: '',      
     database: 'bsit-22',
-    // ... natitirang settings
+    
 };
